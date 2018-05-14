538960116fc883cb305608fa3d83b598428afcc5
Breno Viana <brenomfviana@gmail.com>
2018-05-14 15:03:07 -0300

    Adds the language enum to the domain

---
+++ app/domain/language.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Language(Enum):
+  UNKNOW = ("", ""),
+  DUTCH = ("nl", "Nederlands"),
+  ENGLISH = ("en", "English"),
+  FRENCH = ("fr", "Français"),
+  GERMAN = ("de", "Deutsch"),
+  ITALIAN = ("it", "Italiano"),
+  SPANISH = ("es", "Español"),
+  BRAZILIAN_PORTUGUESE = ("pt_br", "Português Brasileiro");
